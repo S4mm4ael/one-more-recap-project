a23f2ffdebb9504ed15ac86f628cc7348191cb23
Siamion Krapivin <homer1996@gmail.com>
2024-10-09 13:01:38 +0200

    refactor: Update PreloaderComponent.vue and store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-import notify from './notify'
+import repos from "./repos";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    notify
-  }
-})
+    repos,
+  },
+});
