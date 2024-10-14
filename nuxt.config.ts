964ca7a44a81124c56fc4c99bead2f33ca31671c
Siamion Krapivin <homer1996@gmail.com>
2024-10-14 14:12:00 +0200

    refactor: Update devtools and CSS preprocessor options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
-  devtools: { enabled: true },
+  devtools: {enabled: true},
   vite: {
     server: {
       hmr: {
         protocol: "ws",
         host: "localhost",
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          implementation: require("sass"),
+        },
       },
     },
   },
