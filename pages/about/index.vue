7d3f637a34f5b53face06a096023fb349bab3811
Siamion Krapivin <homer1996@gmail.com>
2024-10-14 13:54:16 +0200

    refactor: Update about page layout and component imports

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -2,16 +2,36 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section class="about">
       <div class="container">
-        <h1 class="title"> About my blog: </h1>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
-        <Intro title="Lorem ipsum dolor sit amet"/>
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
-        <img src="@/assets/img/about.jpg" alt="">
-        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut </p>
+        <h1 class="title">About my blog:</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+        <Intro title="Lorem ipsum dolor sit amet" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+        <img src="@/assets/img/about.jpg" alt="" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
       </div>
     </section>
   </div>
 </template>
+
+<script>
+import Intro from "~/components/UI/Intro.vue";
+
+export default {
+  components: {Intro},
+};
+</script>
 
 <style lang="scss">
 .about {
