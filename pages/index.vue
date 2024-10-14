26b8e5a757ffa4017b5cc446968f352fb4775bb0
Siamion Krapivin <homer1996@gmail.com>
2024-10-14 14:05:57 +0200

    refactor: Update component imports and layout in index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,48 @@
 <template>
   <div class="wrapper-content wrapper-content--fixed">
-    <promo />
-    <Intro title="My lasts posts: "/>
+    <Promo />
+    <Intro title="My lasts posts: " />
     <PostsList :posts="posts" />
-    <contacts />
+    <Contacts />
   </div>
 </template>
 
 <script>
-import promo from '@/components/Promo.vue'
-import contacts from '@/components/Contacts.vue'
+import Promo from "@/components/Promo.vue";
+import Contacts from "@/components/Contacts.vue";
+import Intro from "@/components/UI/Intro.vue";
+import PostsList from "@/components/Blog/PostsList.vue";
 
 export default {
   components: {
-    promo,
-    contacts
+    Promo,
+    Contacts,
+    Intro,
+    PostsList,
   },
-  data () {
+  data() {
     return {
       posts: [
         {
           id: 1,
-          title: '1 post',
-          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg'
+          title: "1 post",
+          descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          img: "https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg",
         },
         {
           id: 2,
-          title: '2 post',
-          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg'
+          title: "2 post",
+          descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          img: "https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg",
         },
         {
           id: 3,
-          title: '3 post',
-          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
-          img: 'https://dsvf96nw4ftce.cloudfront.net/images/detailed/2/thundercap-calming-cap-action4.jpg'
-        }
-      ]
-    }
-  }
-}
+          title: "3 post",
+          descr: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+          img: "https://dsvf96nw4ftce.cloudfront.net/images/detailed/2/thundercap-calming-cap-action4.jpg",
+        },
+      ],
+    };
+  },
+};
 </script>
