c49766a66501dd0ee0118e74a0f0394ded9299fe
Siamion Krapivin <homer1996@gmail.com>
2024-10-17 14:43:34 +0200

    refactor: Update store/index.js to use ES6 module syntax

--- store/index.js
+++ store/index.js
@@ -1,23 +1,28 @@
 import axios from "axios";
 
-export default {
-  state: {
-    posts: [],
+export const state = () => ({
+  posts: [],
+});
+
+export const mutations = {
+  setPosts(state, posts) {
+    state.posts = posts;
   },
-  mutations: {
-    setPosts(state, posts) {
-      state.posts = posts;
-    },
+};
+
+export const actions = {
+  async fetchPosts({commit}) {
+    const response = await axios.get("http://localhost:3000/posts");
+    commit("setPosts", response.data);
   },
-  actions: {
-    async fetchPosts({commit}) {
-      const response = await axios.get("http://localhost:3000/posts");
-      commit("setPosts", response.data);
-    },
+  async addPost({commit}, post) {
+    const response = await axios.post("http://localhost:3000/posts", post);
+    commit("setPosts", response.data);
   },
-  getters: {
-    allPosts(state) {
-      return state.posts;
-    },
+};
+
+export const getters = {
+  allPosts(state) {
+    return state.posts;
   },
 };
