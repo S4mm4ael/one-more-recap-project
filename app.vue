5ba1c7b4f11a920666cec640fed723f88006b7cf
Siamion Krapivin <homer1996@gmail.com>
2024-10-21 13:43:31 +0200

    refactor: Remove page transition animation and unused styles

--- app.vue
+++ app.vue
@@ -1,8 +1,6 @@
 <template>
   <NuxtLayout>
-    <transition name="page" mode="out-in">
-      <NuxtPage />
-    </transition>
+    <NuxtPage />
   </NuxtLayout>
 </template>
 
@@ -11,15 +9,3 @@ export default {
   name: "App",
 };
 </script>
-
-<style>
-.page-enter-active,
-.page-leave-active {
-  transition: all 0.5s ease;
-}
-.page-enter,
-.page-leave-to {
-  transform: translateX(10px);
-  opacity: 0;
-}
-</style>
