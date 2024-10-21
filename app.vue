3bf7625e0a51b2242bef416acef8206e715bc65e
Siamion Krapivin <homer1996@gmail.com>
2024-10-21 12:57:29 +0200

    refactor: Add page transition animation to app.vue component

--- app.vue
+++ app.vue
@@ -1,6 +1,8 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <transition name="page" mode="out-in">
+      <NuxtPage />
+    </transition>
   </NuxtLayout>
 </template>
 
@@ -9,3 +11,15 @@ export default {
   name: "App",
 };
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.5s ease;
+}
+.page-enter,
+.page-leave-to {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
